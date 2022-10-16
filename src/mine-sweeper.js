cf82bd575b2a96774b33ae68810f0ecbbcf67315
Dmitry <Reismoos@gmail.om>
2022-10-16 16:33:27 +0300

    finish mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function minesweeper(m) {
+  let result = m.slice(0);
+  console.log('result2', result)
+  let u =[]
+  let c = result.map(el => {
+    console.log('el1', el)
+    let t = []
+    el.map(el2 => {
+      t.push(0)
+      el2 = 0
+      console.log(el2)
+    })
+    u.push(t)
+  })
+/*   result.forEach(el1 => {
+    el1.forEach(el2 => {
+      el2 = 0})
+  }); */
+  console.log('result', u)
+  for (let i = 0; i < m.length; i++){
+    for (let j = 0; j < m[i].length; j++){
+      if (m[i][j] === true){
+        u[i][j] = 1
+        if(i > 0 && j > 0 && m[i-1][j-1] === false){
+          u[i-1][j-1] += 1;
+        }
+        if(i>0 && m[i-1][j] === false){
+          u[i-1][j] += 1;
+        }
+        if(i>0 && m[i-1][j+1] === false){
+          u[i-1][j+1] += 1;
+        }
+        if(j>0 && m[i][j-1] === false){
+          u[i][j-1] += 1;
+        }
+        if(m[i][j+1] === false){
+          u[i][j+1] += 1;
+        }
+        if(j>0 &&m[i+1][j-1] === false){
+          u[i+1][j-1] += 1;
+        }
+        if(m[i+1][j] === false){
+          u[i+1][j] += 1;
+        }
+        if(m[i+1][j+1] === false){
+          u[i+1][j+1] += 1;
+        }
+      }
+
+    }
+  }
+  return u
 }
 
 module.exports = {
